f1792cb7c312798a00c945166fbd3f74139b6669
Somasekar Bathina <sb1454@LIBLT-5S9WLC2.unt.ad.unt.edu>
2023-03-09 23:59:25 -0600

    File Reading

--- com.irs.irengine/src/com/irs/irengine/main/IREngineMain.java
+++ com.irs.irengine/src/com/irs/irengine/main/IREngineMain.java
@@ -1,10 +1,12 @@
 package com.irs.irengine.main;
 
+import com.irs.irengine.textparser.TextParser;
+
 public class IREngineMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TextParser initializeParse = new TextParser();
 	}
 
 }
--- com.irs.irengine/src/com/irs/irengine/textparser/TextParser.java
+++ com.irs.irengine/src/com/irs/irengine/textparser/TextParser.java
@@ -1,5 +1,29 @@
 package com.irs.irengine.textparser;
 
+import java.io.File;
+
 public class TextParser {
+	
+	public TextParser() {
+		super();
+		readFiles();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void readFiles() {
+		File directoryPath = new File("ft911");
+		
+	    //List of all files and directories
+	    File filesList[] = directoryPath.listFiles();
+	    
+	    System.out.println("List of files and directories in the specified directory:");
+	    
+	    for(File file : filesList) {
+	       System.out.println("File name: "+file.getName());
+	       System.out.println("File path: "+file.getAbsolutePath());
+	       System.out.println("Size :"+file.getTotalSpace());
+	       System.out.println(" ");
+	    }
+	}
 
 }
